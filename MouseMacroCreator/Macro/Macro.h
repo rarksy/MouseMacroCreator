b59030d415b297c5b9fd90927983e52298fad77d
Rarksy <rarksy1@outlook.com>
2023-11-21 13:57:22 +1000

    removed redundant keys from validKeys vector

--- MouseMacroCreator/Macro/Macro.h
+++ MouseMacroCreator/Macro/Macro.h
@@ -140,50 +140,10 @@ namespace MacroCore
         // Example: KeyDown 1
         // Example: KeyDown .
 
-        // Letters
-        {"a", 'a'},
-        {"b", 'b'},
-        {"c", 'c'},
-        {"d", 'd'},
-        {"e", 'e'},
-        {"f", 'f'},
-        {"g", 'g'},
-        {"h", 'h'},
-        {"i", 'i'},
-        {"j", 'j'},
-        {"k", 'k'},
-        {"l", 'l'},
-        {"m", 'm'},
-        {"n", 'n'},
-        {"o", 'o'},
-        {"p", 'p'},
-        {"q", 'q'},
-        {"r", 'r'},
-        {"s", 's'},
-        {"t", 't'},
-        {"u", 'u'},
-        {"v", 'v'},
-        {"w", 'w'},
-        {"x", 'x'},
-        {"y", 'y'},
-        {"z", 'z'},
-
-
-        // Numbers
-        {"0", '0'},
-        {"1", '1'},
-        {"2", '2'},
-        {"3", '3'},
-        {"4", '4'},
-        {"5", '5'},
-        {"6", '6'},
-        {"7", '7'},
-        {"8", '8'},
-        {"9", '9'},
-
-        //Modifier Keys
+        // Modifier Keys
         {"Shift", VK_SHIFT},
         {"Control", VK_CONTROL},
+        {"Windows", VK_LWIN},
         {"Alt", VK_MENU},
         {"Enter", VK_RETURN},
         {"Space", VK_SPACE},
