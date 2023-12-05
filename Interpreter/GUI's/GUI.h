1c52b80c0db868e6a9c30cfb0047ce15fdfa89de
Rarksy <rarksy1@outlook.com>
2023-12-05 10:39:16 +1000

    fixed gui folder

---
+++ Interpreter/GUI's/GUI.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <filesystem>
+
+namespace GUI
+{
+    
+    namespace Specific
+    {
+
+        namespace GetSavedMacros
+        {
+            std::vector<std::filesystem::path> Get();
+        }
+
+        namespace LogSavedMacros
+        {
+            bool Run(std::vector<std::filesystem::path> macroPaths);
+        }
+    }
+}
